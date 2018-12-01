29eb484b3d83759db7dc9b0ecec9bd9da9c344c1
danBennettDev <skjolbrot@gmail.com>
2018-12-01 23:38:57 +0000

    autogen: update site1

--- site_publish.bat
+++ site_publish.bat
@@ -1,8 +1,10 @@
 @echo off
 cd /d "%~dp0"
+git --git-dir=.git --work-tree=_site add --all
+git --git-dir=.git --work-tree=_site commit -m "autogen: update site1"
 git --git-dir=.git --work-tree=_site push code
 rmdir _site /s /q
 call bundle exec jekyll build
 git --git-dir=.git --work-tree=_site add --all
-git --git-dir=.git --work-tree=_site commit -m "autogen: update site"
+git --git-dir=.git --work-tree=_site commit -m "autogen: update site2"
 git --git-dir=.git --work-tree=_site push
